062ba31a14148b5934f573718dcbdf3d2b94b4e2
Hitaishi <hitaishi@example.com>
2026-07-21 21:13:08 +0530

    feat-add-spring-boot-backend

---
+++ backend/src/main/java/com/venturo/VenturoApplication.java
@@ -0,0 +1,11 @@
+package com.venturo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenturoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VenturoApplication.class, args);
+    }
+}
